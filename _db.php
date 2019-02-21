18db6215cc76769808eb37e8f546f32c4ddb233f
Rezarestu <reza.restu96@gmail.com>
2019-02-21 21:12:51 +0700

    masuk bro

---
+++ _db.php
@@ -0,0 +1,9 @@
+<?php
+//MSSQL settings 
+$sql_user = 'sa'; // SQL ID
+$sql_pwd = 'bsk46SQL'; // SQL PASSWORD 
+$base = "db_reza"; // SQL BASE name
+$host = "192.168.98.158"; // SQL HOST
+$link = mssql_connect($host, $sql_user, $sql_pwd);
+
+?>
